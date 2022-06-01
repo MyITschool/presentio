583bf0561e34bb3106b2786b4d5310866fc361d5
Evonicgu <evonicgu@gmail.com>
2022-06-01 08:23:26 +0300

    Fixed bug with context switching and SearchRequest serialization

--- app/src/main/java/com/presentio/models/SearchRequest.java
+++ app/src/main/java/com/presentio/models/SearchRequest.java
@@ -1,10 +1,12 @@
 package com.presentio.models;
 
+import java.io.Serializable;
+
 import io.objectbox.annotation.Entity;
 import io.objectbox.annotation.Id;
 
 @Entity
-public class SearchRequest {
+public class SearchRequest implements Serializable {
     @Id
     public long id;
 
